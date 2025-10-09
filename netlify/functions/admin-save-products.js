86f4186df200a41abf6d598caef19ca35ba7c1a5
willyldx <spencer62@protonmail.com>
2025-10-09 22:04:29 +0100

    Update admin-save-products.js

--- netlify/functions/admin-save-products.js
+++ netlify/functions/admin-save-products.js
@@ -6,7 +6,7 @@ const cors = {
   "Access-Control-Allow-Methods": "POST, OPTIONS"
 };
 
-exports.handler = async (event) => {
+export async function handler(event, context) {
   if (event.httpMethod === "OPTIONS") return { statusCode: 204, headers: cors };
   if (event.httpMethod !== "POST")    return { statusCode: 405, headers: cors, body: "Method Not Allowed" };
 
