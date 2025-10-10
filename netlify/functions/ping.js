ad14f66b54ed3bd1c60dea4665dcf7c11061ed95
willyldx <spencer62@protonmail.com>
2025-10-10 23:44:44 +0100

    Delete netlify/functions directory

--- netlify/functions/ping.js
+++
@@ -1,8 +0,0 @@
-// ESM / sans import / sans env
-export const handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ ok: true, ts: Date.now() })
-  };
-};
