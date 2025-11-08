f4f149f1b526f8dc9681f214c7f59d023cbcc0d4
willyldx <spencer62@protonmail.com>
2025-11-08 23:28:30 +0100

    Update ping.js

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,10 +1,20 @@
-export default async () => {
-  return new Response(JSON.stringify({
-    ok: true,
-    env: {
-      SUPABASE_URL: !!process.env.SUPABASE_URL,
-      SUPABASE_SERVICE_ROLE_KEY: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
-      ADMIN_SECRET: !!process.env.ADMIN_SECRET
-    }
-  }), { headers: { "content-type": "application/json" }});
+// netlify/functions/ping.js
+exports.handler = async () => {
+  const { SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, ADMIN_SECRET } = process.env;
+
+  return {
+    statusCode: 200,
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "access-control-allow-origin": "*",
+    },
+    body: JSON.stringify({
+      ok: true,
+      env: {
+        SUPABASE_URL: !!SUPABASE_URL,
+        SUPABASE_SERVICE_ROLE_KEY: !!SUPABASE_SERVICE_ROLE_KEY,
+        ADMIN_SECRET: !!ADMIN_SECRET,
+      },
+    }),
+  };
 };
